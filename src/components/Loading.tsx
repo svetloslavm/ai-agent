81821262a01c4229b87b36303497948cb536207b
svetoslavm <svetoslavm.s8@learn.telerikacademy.com>
2025-05-28 13:53:52 +0300

    Replace text with spinner icon

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 
+import { ImSpinner } from "react-icons/im";
+
 interface LoadingProps {
   isLoading: boolean;
 }
@@ -7,6 +9,6 @@ interface LoadingProps {
 export const Loading: FC<LoadingProps> = ({ isLoading }) =>
   isLoading && (
     <span className="absolute inset-0 flex items-center justify-center bg-white/70 z-10 text-xs">
-      Loading...
+      <ImSpinner size={28} className="animate-spin" />
     </span>
   );
