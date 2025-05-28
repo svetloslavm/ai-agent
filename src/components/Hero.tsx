2446a1f22d645acbc8fa6c1343eabe298e0f127d
svetoslavm <svetoslavm.s8@learn.telerikacademy.com>
2025-05-28 08:14:39 +0300

    Added GSAP animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,36 @@
-import { BsRobot } from "react-icons/bs";
+"use client";
 
-export const Hero = () => (
-  <div className="flex flex-col gap-3 m-auto justify-center text-center">
-    <h1 className="sr-only">Smart and fast AI Agent</h1>
-    <BsRobot className="m-auto text-foreground/80" size={80} />
-    <p className="text-3xl lg:text-4xl px-4 font-bold text-foreground/80">
-      &lt; Smart. Fast. &gt; <br /> Always here to help.
-    </p>
-    <p className="text-sm md:text-lg px-4 text-foreground/80">
-      Whether you need answers, support, or guidance - just ask.
-    </p>
-    <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-  </div>
-);
+import { useEffect, useRef } from "react";
+
+import { BsRobot } from "react-icons/bs";
+import { useAnimation } from "@/hooks";
+
+export const Hero = () => {
+  const textRef = useRef<HTMLDivElement>(null);
+
+  const { animateScrambleText } = useAnimation();
+
+  useEffect(() => {
+    if (!textRef.current) return;
+
+    animateScrambleText(textRef.current, "{ Smart & Fast }");
+  }, [textRef, animateScrambleText]);
+
+  return (
+    <div className="flex flex-col gap-3 m-auto justify-center text-center">
+      <h1 className="sr-only">Smart and fast AI Agent</h1>
+      <BsRobot className="m-auto text-foreground/80" size={80} />
+      <p
+        ref={textRef}
+        className="text-2xl md:text-3xl px-4 font-bold text-foreground/80 h-8"
+      />
+      <p className="text-2xl md:text-3xl px-4 font-bold text-foreground/80">
+        Always here to help.
+      </p>
+      <p className="text-xs md:text-lg px-4 text-foreground/80">
+        Whether you need answers, support, or guidance - just ask.
+      </p>
+      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    </div>
+  );
+};
---
+++ src/hooks/useAnimation.ts
@@ -0,0 +1,28 @@
+import gsap from "gsap";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+
+gsap.registerPlugin(ScrambleTextPlugin);
+
+export const useAnimation = () => {
+  const animateScrambleText = (
+    textRef: HTMLDivElement,
+    text: string,
+    rightToLeft: boolean = false
+  ) => {
+    if (!textRef) return;
+
+    gsap.to(textRef, {
+      duration: 2.5,
+      scrambleText: {
+        text,
+        chars: "█▓▒░",
+        speed: 0.25,
+        revealDelay: 0.5,
+        tweenLength: false,
+        rightToLeft,
+      },
+    });
+  };
+
+  return { animateScrambleText };
+};
